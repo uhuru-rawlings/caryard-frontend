0716cf56418d0c0d5b101b2ece92cfc7b9c34e8a
uhururawlings40 <uhururawlings40@gmail.com>
2022-05-05 10:46:05 +0300

    added caryard routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { HomepageComponent } from './homepage/homepage.component';
+import { CarsComponent } from './cars/cars.component';
+import { DetailsComponent } from './details/details.component';
+import { MessagesComponent } from './messages/messages.component';
+import { UploadsComponent } from './uploads/uploads.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { ResetpasswordComponent } from './resetpassword/resetpassword.component';
+const routes: Routes = [
+{path:'',component:HomepageComponent},
+{path:'/cars',component:CarsComponent},
+{path:'/car/details',component:DetailsComponent},
+{path:'/user/messages',component:MessagesComponent},
+{path:'/user/uploads',component:UploadsComponent},
+{path:'/user/profile',component:ProfileComponent},
+{path:'/user/contact',component:ContactsComponent},
+{path:'/user/login',component:LoginComponent},
+{path:'/user/register',component:SignupComponent},
+{path:'/user/reset_password',component:ResetpasswordComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { CarsComponent } from './cars/cars.component';
+import { DetailsComponent } from './details/details.component';
+import { MessagesComponent } from './messages/messages.component';
+import { UploadsComponent } from './uploads/uploads.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { ResetpasswordComponent } from './resetpassword/resetpassword.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    CarsComponent,
+    DetailsComponent,
+    MessagesComponent,
+    UploadsComponent,
+    ProfileComponent,
+    ContactsComponent,
+    NavbarComponent,
+    LoginComponent,
+    SignupComponent,
+    ResetpasswordComponent
   ],
   imports: [
     BrowserModule,

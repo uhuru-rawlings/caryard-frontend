6619a544d234c2402fbc731bed73859e3a162c7b
uhururawlings40 <uhururawlings40@gmail.com>
2022-05-09 23:37:58 +0300

    added login details

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -8,31 +8,11 @@ import { CookieService } from 'ngx-cookie-service';
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent implements OnInit {
-  error:any = ''
-  useremail:any = ''
-  password:any = ''
+
   constructor(private LoginService:LoginService, private cookie:CookieService) { }
 
   ngOnInit(): void {
   }
   
-  loginuser(){
-    if(this.useremail ==='' || this.password === ''){
-      alert("please fill out all required fields")
-      return
-    }else{
-      let details = {
-        "useremail":this.useremail,
-        "password":this.password
-      }
-
-      this.LoginService.loginuser(details).subscribe((data) => {
-        if(typeof(data) == "string"){
-          this.error = data
-        }else{
-          this.cookie.set("jwt",JSON.stringify(data.jwt))
-        }
-      })
-    }
-  }
+  
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
+import { LoginService } from '../services/login.service';
+
 
 @Component({
   selector: 'app-navbar',
@@ -9,7 +11,10 @@ import { CookieService } from 'ngx-cookie-service';
 export class NavbarComponent implements OnInit {
   login:any = ''
   button:any = 'f'
-  constructor(private cookie:CookieService) { }
+  error:any = ''
+  useremail:any = ''
+  password:any = ''
+  constructor(private LoginService:LoginService, private cookie:CookieService) { }
 
   ngOnInit(): void {
     this.toogleLogin()
@@ -25,5 +30,25 @@ export class NavbarComponent implements OnInit {
   toogleLogin(){
     let user = this.cookie.get("userd")
     {user ? this.button = "Logout": this.button = ''}
+  }
+
+  loginuser(){
+    if(this.useremail ==='' || this.password === ''){
+      alert("please fill out all required fields")
+      return
+    }else{
+      let details = {
+        "useremail":this.useremail,
+        "password":this.password
+      }
+
+      this.LoginService.loginuser(details).subscribe((data) => {
+        if(typeof(data) == "string"){
+          this.error = data
+        }else{
+          this.cookie.set("jwt",JSON.stringify(data.jwt))
+        }
+      })
+    }
   }
 }

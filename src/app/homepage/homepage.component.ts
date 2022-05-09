ef0418f2c158cdb018a29c2c8770af29593490d1
uhururawlings40 <uhururawlings40@gmail.com>
2022-05-09 23:34:42 +0300

    added login function

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../services/login.service';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-homepage',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent implements OnInit {
-
-  constructor() { }
+  error:any = ''
+  useremail:any = ''
+  password:any = ''
+  constructor(private LoginService:LoginService, private cookie:CookieService) { }
 
   ngOnInit(): void {
   }
+  
+  loginuser(){
+    if(this.useremail ==='' || this.password === ''){
+      alert("please fill out all required fields")
+      return
+    }else{
+      let details = {
+        "useremail":this.useremail,
+        "password":this.password
+      }
 
+      this.LoginService.loginuser(details).subscribe((data) => {
+        if(typeof(data) == "string"){
+          this.error = data
+        }else{
+          this.cookie.set("jwt",JSON.stringify(data.jwt))
+        }
+      })
+    }
+  }
 }
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+  url:string = 'http://127.0.0.1:8000/api/login/'
+  constructor(private http:HttpClient) { }
 
-  constructor() { }
+  loginuser(details:any):Observable<any>{
+    return this.http.post(this.url, details)
+  }
 }

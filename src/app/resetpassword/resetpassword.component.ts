6f4dfe5302d08faf4a47dd30908c612b5f72ff87
uhururawlings40 <uhururawlings40@gmail.com>
2022-05-10 00:43:20 +0300

    addede reset password data binding

--- src/app/resetpassword/resetpassword.component.ts
+++ src/app/resetpassword/resetpassword.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ResetService } from '../services/reset.service';
 @Component({
   selector: 'app-resetpassword',
   templateUrl: './resetpassword.component.html',
@@ -8,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 export class ResetpasswordComponent implements OnInit {
   useremail:any = ''
   password:any = ''
-  constructor() { }
+  error:any =''
+  success:any =''
+  constructor(private ResetService:ResetService) { }
 
   ngOnInit(): void {
   }
+  reset(){
+    if(this.useremail === '' || this.password === ''){
+      alert('please fill all required fields')
+      return
+    }else{
+      let details = {
+        "useremail":this.useremail,
+        "password":this.password
+      }
 
+      this.ResetService.resetpassword(details).subscribe((data) => {
+        if(data = "user dont exist."){
+          this.error = data
+        }else{
+          this.success = data
+        }
+      })
+    }
+  }
 }
--- src/app/services/reset.service.ts
+++ src/app/services/reset.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ResetService {
+  url:string = "http://127.0.0.1:8000/api/reset/"
+  constructor(private http:HttpClient) { }
 
-  constructor() { }
+  resetpassword(details:any):Observable<any>{
+    return this.http.post(this.url, details)
+  }
 }

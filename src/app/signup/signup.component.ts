92ecbf467512d1e04791d78dfaef388e622e3e3b
uhururawlings40 <uhururawlings40@gmail.com>
2022-05-09 22:33:02 +0300

    added user details variables

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-
+  username:any = ''
+  useremail:any = ''
+  password:any = ''
   constructor() { }
 
   ngOnInit(): void {

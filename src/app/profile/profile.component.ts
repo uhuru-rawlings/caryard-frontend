dbee6cbdac5d10f130fe26cc5932c89cbaf51d87
uhururawlings40 <uhururawlings40@gmail.com>
2022-05-10 14:43:57 +0300

    added user profile

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -10,21 +10,41 @@ import { Router } from '@angular/router'
 })
 export class ProfileComponent implements OnInit {
   userdet:any;
+  initials:any;
   constructor(private cookie:CookieService, private user:UserService, private route:Router) { }
 
   ngOnInit(): void {
+    this.getuser()
+    
   }
   getuser(){
     let token = this.cookie.get("jwt")
+    
+    if (token){
     let tokens = {
       "jwt": JSON.parse(token)
     }
-    if (token){
      this.user.decriptuser(tokens).subscribe((data) => {
        if(typeof(data) != "string"){
         this.userdet = data
+        console.log(this.userdet)
+        this.userinit()
        }
      })
+    }else{
+      this.route.navigate(['/'])
+    }
+  }
+  userinit(){
+    if(this.userdet){
+      let texts = this.userdet.username
+      let array = texts.split(" ")
+      if(array.length > 1){
+          this.initials = array[0]+ "." + array[1]
+      }else{
+        this.initials = array[0]
+      }
+      console.log(array)
     }else{
       this.route.navigate(['/'])
     }
